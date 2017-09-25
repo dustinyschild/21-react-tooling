1a558822445d440c5727b64ceabf100a556a5d6f
Dustin <dyelland@gmail.com>
2017-09-25 14:53:37 -0500

    more set up

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,45 @@ const path = require('path');
 
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {test: /\.scss$/, use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader','sass-loader'],
-                publicPath: '/dist'
-            })
-            },
-            {
-                test: /\.js$/,
-                exclude:  /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000,
-        stats: 'errors-only'
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Web Thingy Demo',
-          minify: {
-              collapseWhitespace: true
-          },
-          template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
-        }),
-        new ExtractTextPlugin({
-            filename: 'app.css',
-            disable: false,
-            allChunks: true
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','sass-loader'],
+          publicPath: '/dist'
         })
-      ]
+      },
+      {
+        test: /\.js$/,
+        exclude:  /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    stats: 'errors-only'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Web Thingy Demo',
+        minify: {
+          collapseWhitespace: true
+        },
+      template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disable: false,
+      allChunks: true
+    })
+  ]
 }
