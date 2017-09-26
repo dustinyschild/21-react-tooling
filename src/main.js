163659583c1bcf4607b97322b2bc09bbc8a036a0
Dustin <dyelland@gmail.com>
2017-09-26 00:11:37 -0500

    renders different cows on button click!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const css = require('./style/main.scss');
-const { say } = require('cowsay');
-const { SQUIRREL } = require('cowsay');
+const { say,SQUIRREL,DRAGON } = require('cowsay');
 const { fake } = require('faker');
 
 import React from 'react';
@@ -13,8 +12,7 @@ class Main extends React.Component {
       content: {
         text: 'grazing in the browser',
         cow: '',
-        eyes: 'pp',
-        tongue: 'U'
+        cowType: 'cow'
       }
     }
     this.newSaying = this.newSaying.bind(this);
@@ -25,36 +23,48 @@ class Main extends React.Component {
     this.setState(() => {
       this.state.content.text = fake("{{lorem.sentence}}");
       console.log(this.state.content.text);
-      return { state: { content: this.state.content.text }};
     })
   }
 
   newCow(){
     this.setState(() => {
-      var dropDown = document.getElementById('selected');
-      console.log(dropDown);
+      var dropDown = document.getElementById('select');
+      console.log(dropDown.value);
+      this.state.content.cowType = dropDown.value;
+
+      switch (dropDown.value){
+        case 'cow':
+          this.state.content.cow = '';
+        break;
+        case 'squirrel':
+        this.state.content.cow = SQUIRREL;
+        break;
+        case 'dragon':
+        this.state.content.cow = DRAGON;
+        break;
+      }
     })
   }
 
   render(){
     return (
       <div>
-      <pre>
-        Content:
-          text: {this.state.content.text}
-          cow: {this.state.content.cow}
-      </pre>
-      <pre>{
-          say(this.state.content)
-      }</pre>
-      <button onClick={this.newSaying}>New Ipsum!</button>
-      <button onClick={this.newCow}>New Cow!</button>
-      <select id="select">
-        <option value="cow">Cow</option>
-        <option value="squirrel">Squirrel</option>
-        <option value="dragon">Dragon</option>
-      </select>
-    </div>
+        <div>
+          Content:
+            <p>text: {this.state.content.text}</p>
+            <p>cowType: {this.state.content.cowType}</p>
+        </div>
+        <pre>{
+            say(this.state.content)
+        }</pre>
+        <button onClick={this.newSaying}>New Ipsum!</button>
+        <select id="select">
+          <option value="cow">Cow</option>
+          <option value="squirrel">Squirrel</option>
+          <option value="dragon">Dragon</option>
+        </select>
+        <button onClick={this.newCow}>New Cow!</button>
+      </div>
     )
   }
 }
